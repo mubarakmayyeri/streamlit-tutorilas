9467b253a2430f5c0f3ab854c4d0becd05e5d805
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-02-22 10:54:53 +0530

    app finished

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mubar_dyaglyf\OneDrive\Documents\Projects\data_scienece\streamlit_tutorial\env
+set VIRTUAL_ENV=C:\Users\mubar_dyaglyf\OneDrive\Documents\Projects\data_scienece\web_applications\streamlit_tutorial\env
 
 if not defined PROMPT set PROMPT=$P$G
 
