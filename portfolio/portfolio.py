9e859529f415b971dcc0f351e2f4291601a1949e
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-02-26 09:15:24 +0530

    portifolio started

---
+++ portfolio/portfolio.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# More emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title='Mubarak Mayyeri', page_icon=':wink:', layout='wide')
+
+# Header Section
+st.subheader("Hi, I am Mubarak Mayyeri :wave:")
+st.title("Data Scientist | Python Developer | Web Designer")
+st.write("As a skilled data scientist with a passion for statistics, machine learning, and web development, I thrive on extracting insights from complex data sets."
+         "I specialize in translating data into actionable insights for data-driven decisions,"
+         "and help businesses make informed, data-driven decisions that lead to success.")
+st.write("[Connect with me on LinkedIn >](https://www.linkedin.com/in/mubarakmayyeri/)")
+st.write("[Checkout my GitHub Profile >](https://github.com/mubarakmayyeri)")
